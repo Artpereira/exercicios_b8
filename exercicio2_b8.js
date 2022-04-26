195343f3bfc0c203bd67f0f5d8d10e918e6ac7bd
Art <art_dev@outlook.com>
2022-04-26 19:14:16 -0300

    reforço em exercicios com for

--- exercicio2_b8.js
+++ exercicio2_b8.js
@@ -1,5 +1,5 @@
 const rightAnswer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const studentAnswer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswer = ['A', 'C', 'N.A', 'D', 'A', 'A', 'D', 'N.A', 'D', 'C'];
 let somaPontos = 0;
 
 
@@ -12,9 +12,7 @@ rightAnswer.forEach((respostaValida, index) => {
     } else {
         somaPontos = somaPontos - 0.5;
     }
-    
-
-})
+    })
 
 console.log(somaPontos);
 
