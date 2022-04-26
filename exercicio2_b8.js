0fa89804ac894a8ecb559fb088960795ab478faa
Art <art_dev@outlook.com>
2022-04-26 18:52:09 -0300

    finalização exercicio 8_2

---
+++ exercicio2_b8.js
@@ -0,0 +1,20 @@
+const rightAnswer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+let somaPontos = 0;
+
+
+
+
+
+rightAnswer.forEach((respostaValida, index) => {
+    if (rightAnswer[index] === studentAnswer[index]) {
+        somaPontos += 1
+    } else {
+        somaPontos = somaPontos - 0.5;
+    }
+    
+
+})
+
+console.log(somaPontos);
+
