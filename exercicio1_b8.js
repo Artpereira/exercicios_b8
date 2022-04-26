89d06f1fe4918a7f81414f151239481f8f457937
Art <art_dev@outlook.com>
2022-04-25 21:58:03 -0300

    teste exercicio1

---
+++ exercicio1_b8.js
@@ -0,0 +1,15 @@
+const newEmployeess =  objeto() 
+    nomeCompleto:"Arthur Pereira",
+    email:"art_dev@outlook.com",
+
+
+
+        function newEmployees() {
+            const employees = {
+                id1: '',
+                id2: '',
+                id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+            };
+            return employees;
+        }
+// console.log(newEmployee);
---
+++ teste.js
@@ -0,0 +1,19 @@
+const employeeObject = () => {
+    const worker = {
+        nome: "Arthur Pereira",
+        email: "arthureletrica@outlook.com"
+    }
+    return worker
+}
+
+
+const newEmployees = () => {
+    const employees = {
+      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+    };
+
+    console.log(employeeObject());
