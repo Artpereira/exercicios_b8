6ba4b5817439d5bec2272fa2306389a05e92d82a
Art <art_dev@outlook.com>
2022-04-27 23:13:09 -0300

    aprendizado sobre reduce exercicio 8.15.1

--- exercicios_8.14.js
+++ exercicios_8.14.js
@@ -5,17 +5,27 @@
 // const bigger = numbers.reduce(getBigger, 0);
 // console.log(bigger); // 85
 
-const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 
-const getEven = (number) => number % 2 === 0;
+// const getEven = (number) => number % 2 === 0;
 // confere se o valor que for passado para o parâmetro number é divisível por 2
 
-const sumPair = (valorAtual, valorFinal) => valorAtual + valorFinal;
+// const sumPair = (valorAtual, valorFinal) => valorAtual + valorFinal;
 // faz uma soma de dois parâmetros que serão passados como valorAtual e valorFinal
 
-const sumNumbers = (item) => item.filter(getEven).reduce(sumPair)
+// const sumNumbers = (item) => item.filter(getEven).reduce(sumPair)
 // armazena na função as execuções das funções passadas acima 
 
-console.log(sumNumbers(numbers));
+// console.log(sumNumbers(numbers));
 // sumNumbers recebe o array como parâmetro para todas as outras funções
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPair = (currentValue, number) => (
+  (number % 2 === 0) ? currentValue + number : currentValue
+);
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+console.log(sumNumbers(numbers)); // 152
