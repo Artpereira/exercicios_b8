a829bc740b7faaff28456445b38d85ac52cd2e45
Art <art_dev@outlook.com>
2022-04-29 13:13:57 -0300

    exercicio callback

---
+++ exercicio3_bloco10.js
@@ -0,0 +1,4 @@
+const myList = [1, 2, 3];
+
+const [item = item2, item2, item3 = item] = myList
+console.log(myList);
---
+++ exercicio4_bloco10.js
@@ -0,0 +1,11 @@
+// Agora, vamos ver um pouco da prática realizando este exercício de fixação. Copie o código para ser executado na sua máquina:
+// Copiar
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers);
