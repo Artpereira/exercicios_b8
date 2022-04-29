2946d798b9698b209745a9653bb148f4c8755e85
Art <art_dev@outlook.com>
2022-04-29 19:14:13 -0300

    aprendizado sobre callbacks

---
+++ exercicio7_bloco10.js
@@ -0,0 +1,37 @@
+const despesas = [
+    {
+      gym: 99,
+    },
+    {
+      ifood: 200,
+    },
+    {
+      phone: 60,
+    },
+    {
+      internet: 100,
+    },
+  ];
+  
+  const renda = 1000;
+
+  const despesaMensal = (renda, despesas, callback) => {
+    //   callback é a função somaDespesas
+
+    const despesaTotal = callback(despesas);
+    // aqui callback (somaDespesas) recebe o parâmetro despesas que nada mais é do que o objeto contigo na variável "despesas"
+    const saldoFinal = renda - despesaTotal;
+  
+    console.log(`Balanço do mês:
+      Recebido: R$${renda},00
+      Gasto: R$${despesaTotal},00
+      Saldo: R$${saldoFinal},00 `);
+  };
+
+  const somaDespesas = (despesas) => {
+    const custoItem = despesas.map((item) => Object.values(item));
+    const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+    return despesaTotal;
+  };
+  
+  despesaMensal(renda, despesas, somaDespesas);
